ef126019fd8b8e30ef25a2bae9239e52ea0aa19b
khusnikhorlan <khusni.khorlan@gmail.com>
2020-12-19 00:25:49 +0600

    5lesson_bonus

---
+++ app/src/main/java/com/example/contextmenu4bonus/lesson5_bonus.java
@@ -0,0 +1,14 @@
+package com.example.contextmenu4bonus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lesson5_bonus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson5_bonus);
+    }
+}
